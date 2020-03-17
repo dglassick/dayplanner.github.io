cf904c7b0a112c4ae871caf7567456582a14f533
Derek Glassick <derek.glassick@gmail.com>
2020-03-17 09:56:38 -0700

    accidentally had variable that said hour instead of time, messing up last function. changed some of the css colors.

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,6 @@ var hour12 = 1;
 var hour24 = moment().format("H");
 var hour12 = moment().format("h");
 
-;
 // this function will create divs based on the hour
 function createBoxes() {
   // for loop will have divs start at 9 and end at 5
@@ -43,38 +42,38 @@ function createBoxes() {
       meridiem = "am";
     }
     // adds the variables timeBox and meridiem together in the span
-    $hourDisplay.text(timeBox +" "+ meridiem);
-    console.log($hourDisplay.value)
+    $hourDisplay.text(timeBox + " " + meridiem);
+    console.log($hourDisplay.value);
 
     $rowGrid.append($column2);
     $column2.append($hourDisplay);
 
     // 2nd column that allows you to input what you're going to do at a particular time
-    var $hourlyPlan = $('<input>');
+    var $hourlyPlan = $("<input>");
 
-    $hourlyPlan.attr('id', `input-${workHour}`);
-    $hourlyPlan.attr('time-index', workHour);
-    $hourlyPlan.attr('type', 'text');
-    $hourlyPlan.attr('class', 'dailyPlan');
+    $hourlyPlan.attr("id", `input-${workHour}`);
+    $hourlyPlan.attr("time-index", workHour);
+    $hourlyPlan.attr("type", "text");
+    $hourlyPlan.attr("class", "dailyPlan");
 
     // access index from data array for hour
     $hourlyPlan.val(planArr[workHour]);
 
     //creates the middle column
-    var $column9 = $('<div>');
-    $column9.addClass('col-md-9');
+    var $column9 = $("<div>");
+    $column9.addClass("col-md-9");
 
     $rowGrid.append($column9);
     $column9.append($hourlyPlan);
 
     // creates the final column
-    var $column1 = $('<div>');
-    $column1.addClass('col-md-1');
-    
-    var $saveButton = $('<i>');
-    $saveButton.attr('id', `saveid-${workHour}`);
-    $saveButton.attr('save-id', workHour);
-    $saveButton.attr('class', 'far fa-save saveIcon');
+    var $column1 = $("<div>");
+    $column1.addClass("col-md-1");
+
+    var $saveButton = $("<i>");
+    $saveButton.attr("id", `saveid-${workHour}`);
+    $saveButton.attr("save-id", workHour);
+    $saveButton.attr("class", "far fa-save saveIcon");
 
     $rowGrid.append($column1);
     $column1.append($saveButton);
@@ -82,22 +81,18 @@ function createBoxes() {
     colorChanger($rowGrid, time);
 
     $dayPlannerEl.append($rowGrid);
-    
   }
 }
 
-function colorChanger($rowColor, time){
-    if(time < hour24){
-        $rowColor.css('background-color', 'lightgrey')
-    }
-    else if (hour > hour24){
-        $rowColor.css('background-color', 'green')
-    }
-    else {
-        $rowColor.css('background-color', 'red')
-    }
-};
-
+function colorChanger($rowColor, time) {
+  if (time < hour24) {
+    $rowColor.css("background-color", "lightgrey");
+  } else if (time > hour24) {
+    $rowColor.css("background-color", "lightblue");
+  } else {
+    $rowColor.css("background-color", "coral");
+  }
+}
 
 function savePlans() {
   var savedPlans = JSON.parse(localStorage.getItem("savedPlans"));
@@ -113,31 +108,29 @@ function savePlans() {
   }
   console.log(planArr);
   console.log(savedPlans);
-};
+}
 
-$(document).on('click', 'i', function(event){
-event.preventDefault();
+$(document).on("click", "i", function(event) {
+  event.preventDefault();
 
-var $index = $(this).attr('save-id');
+  var $index = $(this).attr("save-id");
 
-var inputText = '#input-'+$index;
-var $value = $(inputText).val();
+  var inputText = "#input-" + $index;
+  var $value = $(inputText).val();
 
-planArr[$index] = $value;
+  planArr[$index] = $value;
 
-$(`#saveid-${$index}`).removeClass('zoomIn');
-localStorage.setItem('savedPlans', JSON.stringify(planArr));
+  $(`#saveid-${$index}`).removeClass("zoomIn");
+  localStorage.setItem("savedPlans", JSON.stringify(planArr));
 });
 
-$(document).on('change', 'input', function(event){
-event.preventDefault();
+$(document).on("change", "input", function(event) {
+  event.preventDefault();
 
-let i = $(this).attr('time-index');
+  let i = $(this).attr("time-index");
 
-$(`#saveid-${i}`).addClass('zoomIn');
-})
-
-
+  $(`#saveid-${i}`).addClass("zoomIn");
+});
 
 savePlans();
 createBoxes();
